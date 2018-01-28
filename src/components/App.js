ea8df3c8244c59ff243241866d954995a69c6e8a
Stefan Bauer <Stefan@SJB-MacBook.fritz.box>
2018-01-28 11:43:59 +0100

    Added Header to Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 import React from 'react'
+
 import Header from './Header'
 import Main from './Main'
 
+import Home from './Home'
+import Import from './import/Import'
+import Transactions from './Transactions'
+
+import { Switch, Route } from 'react-router-dom'
 
 const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-)
+	<div>
+		<Switch>
+			<AppRoute exact path="/" component={Home} />
+			<AppRoute exact path='/import' component={Import} />
+			<AppRoute exact path='/transactions' component={Transactions} />
+		</Switch>
+
+	
+	</div>
+	)
+
+// Create default layout
+// Adding header to each page by rendering multiple components  
+const AppRoute = ({ component: Component, ...rest }) => {
+	return (
+		<Route {...rest} render={matchPropos => (
+			<div>
+				<header><Header /></header>
+				<main><Component {...matchPropos} /></main>
+			</div>
+		)} />
+	)
+}
 
 export default App
